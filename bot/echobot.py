befe30ae74210a1143b28662477f944e4c1bb345
J <joffre@pm.me>
2023-12-02 16:16:54 +0100

    Echobot with return first line of a file

--- bot/echobot.py
+++ bot/echobot.py
@@ -58,6 +58,21 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
 
+async def read_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user a read file."""
+    attachment_file = await update.message.document.get_file()
+
+    tmp_file = "tmp/attachment.txt"
+    await attachment_file.download_to_drive(tmp_file)
+
+    # read txt
+    with open(tmp_file, 'r') as file:
+        # Read the first line
+        first_line = file.readline()
+
+
+    await update.message.reply_document(tmp_file, caption=f"The first line: {first_line}")
+
 
 def main() -> None:
     """Start the bot."""
@@ -67,6 +82,11 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+
+
+    application.add_handler(
+        MessageHandler(filters.ATTACHMENT & ~filters.COMMAND, read_file, block=True)
+    )
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
