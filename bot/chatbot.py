4d52ec1bc01c12b7551f79b490d590d8eeb25372
J <joffre@pm.me>
2023-12-03 00:36:45 +0100

    change basic text

--- bot/chatbot.py
+++ bot/chatbot.py
@@ -96,7 +96,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+        "Bye!", reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END
