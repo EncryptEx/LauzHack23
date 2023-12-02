fa12cd2c2d0883a88fb7637b713089a679db92ca
J <joffre@pm.me>
2023-12-02 21:42:25 +0100

    chatbot es el bueno, pero no funciona, tiene estados pero no funciona del todo

--- bot/echobot.py
+++ bot/echobot.py
@@ -23,8 +23,15 @@ import os
 # Load environment variables from the .env file
 load_dotenv()
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import ForceReply, Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
 TELEGRAM_KEY = os.getenv("TELEGRAM_KEY")
 
@@ -38,15 +45,23 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
+ANALYZE, TEXT_FILE = range(2)
+
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
+    reply_keyboard = [["Analyze", "Text", "Other"]]
     user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+    presentation = rf"Hi! {user.first_name} My name is OpenLogsLauz Bot. I can analyze a few logs files"
+    
+    await update.message.reply_text(
+        rf"{presentation}",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Analyze"
+        )
     )
+    return ANALYZE
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -56,10 +71,12 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 async def read_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user a read file."""
+    await update.message.reply_text("Upload your log file!")
     attachment_file = await update.message.document.get_file()
 
     tmp_file = "tmp/attachment.txt"
     await attachment_file.download_to_drive(tmp_file)
+
 
     # read txt
     with open(tmp_file, 'r') as file:
@@ -71,9 +88,18 @@ async def read_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await update.message.reply_document(tmp_file, caption=f"The first line: {first_line}")
 
+    return TEXT_FILE
+
 async def analyze_log() -> None:
     "Analyze a log and returns some features"
 
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
 
 def main() -> None:
     """Start the bot."""
@@ -91,6 +117,23 @@ def main() -> None:
     application.add_handler(
         MessageHandler(filters.ATTACHMENT & ~filters.COMMAND, read_file, block=True)
     )
+
+    # Add conversation handler with the states FUNCTION, TEXT
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            ANALYZE: [MessageHandler(filters.Regex("^(Analyze|Analyse)$"), read_file)],
+            TEXT_FILE: [MessageHandler(filters.ATTACHMENT & ~filters.COMMAND, cancel)]
+            #LOCATION: [
+            #    MessageHandler(filters.LOCATION, location),
+            #    CommandHandler("skip", skip_location),
+            #],
+            #BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+
 
 
     # Run the bot until the user presses Ctrl-C
